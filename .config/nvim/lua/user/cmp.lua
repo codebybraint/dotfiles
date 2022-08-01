107af79e89c9f9877e2bdcdd3b8991a757c02420
codebybraint <codeby.braint@gmail.com>
2022-08-01 22:01:07 +0700

    change icons and add tabnine percentage

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -13,41 +13,54 @@ if not tabnine_status_ok then
   return
 end
 
+local lspkind_status_ok, lspkind = pcall(require, "lspkind")
+if not lspkind_status_ok then
+    return
+end
 require("luasnip/loaders/from_vscode").lazy_load()
 
+local compare = require "cmp.config.compare"
 
---local check_backspace = function()
-  --local col = vim.fn.col "." - 1
-  --return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
---end
+local check_backspace = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
 
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-  Text = "text ",
-  Method = "methhod m",
-  Function = "function ",
-  Constructor = "constructor ",
-  Field = "field ",
-  Variable = "variable ",
-  Class = "class ",
-  Interface = "interface ",
-  Module = "module ",
-  Property = "property ",
-  Unit = "unit ",
-  Value = "value ",
-  Enum = "enum ",
-  Keyword = "keyword ",
-  Snippet = "snippet ",
-  Color = "color ",
-  File = "file ",
-  Reference = "reference ",
-  Folder = "folder ",
-  EnumMember = "enum member ",
-  Constant = "constant ",
-  Struct = "struct ",
-  Event = "event ",
-  Operator = "operator ",
-  TypeParameter = "type parameter ",
+  Text = " ",
+  Method = " m",
+  Function = " ",
+  Constructor = " ",
+  Field = " ",
+  Variable = " ",
+  Class = " ",
+  Interface = " ",
+  Module = " ",
+  Property = " ",
+  Unit = " ",
+  Value = " ",
+  Enum = " ",
+  Keyword = " ",
+  Snippet = " ",
+  Color = " ",
+  File = " ",
+  Reference = " ",
+  Folder = " ",
+  EnumMember = " ",
+  Constant = " ",
+  Struct = " ",
+  Event = " ",
+  Operator = " ",
+  TypeParameter = " ",
+}
+
+local source_mapping = {
+	buffer = "[Buffer]",
+	nvim_lsp = "[LSP]",
+	nvim_lua = "[Lua]",
+	cmp_tabnine = "[TN]",
+	path = "[Path]",
 }
 
 vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
@@ -56,42 +69,91 @@ vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
 vim.api.nvim_set_hl(0, "CmpItemKindCrate", { fg = "#F64D00" })
 
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
+    snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
 
-  mapping = cmp.mapping.preset.insert {
+    mapping = cmp.mapping.preset.insert {
+        -- ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+        -- ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }), --currently i use Ctrl+j for split pane
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
-        ["<C-y>"] = cmp.mapping.confirm({
+        ["<CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
         }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.jumpable(1) then
+            luasnip.jump(1)
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif check_backspace() then
+            -- cmp.complete()
+            fallback()
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),  
   },
   formatting = {
     fields = { "abbr", "kind", "menu" },
-    format = function(entry, vim_item)
-      -- NOTE: order matters
-        vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-        vim_item.menu = ({
-                buffer = "[buf]",
-                nvim_lsp = "[LSP]",
-                nvim_lua = "[api]",
-                path = "[path]",
-                luasnip = "[snip]",
-                cmp_tabnine = "[tN]",
-                copilot = "[copilot]",
-            })
-        [entry.source.name]
-      return vim_item
-    end,
+    -- format = function(entry, vim_item)
+    --   -- NOTE: order matters
+    --     vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+    --     vim_item.menu = ({
+    --             buffer = "[buf]",
+    --             nvim_lsp = "[LSP]",
+    --             nvim_lua = "[api]",
+    --             path = "[path]",
+    --             luasnip = "[snip]",
+    --             cmp_tabnine = "[tN]",
+    --             copilot = "[copilot]",
+    --         })
+    --     [entry.source.name]
+    --   return vim_item
+    -- end,
+    format = lspkind.cmp_format({
+      mode = "symbol_text", -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+      maxwidth = 40, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      before = function(entry, vim_item)
+        vim_item.kind = string.format("%s %s", vim_item.kind, kind_icons[vim_item.kind])
+        local menu = source_mapping[entry.source.name]
+        if entry.source.name == "cmp_tabnine" then
+          if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+            menu = entry.completion_item.data.detail .. " " .. menu
+          end
+          vim_item.kind = ""
+        end
+        vim_item.menu = menu
+
+        return vim_item
+      end,
+    }),
   },
   sources = {
-    { name = "crates", group_index = 1 },
     {
       name = "nvim_lsp",
       filter = function(entry, ctx)
@@ -102,18 +164,27 @@ cmp.setup {
           return true
         end
       end,
-      group_index = 2,
     },
-    { name = "nvim_lua", group_index = 2 },
-    { name = "copilot", keyword_length = 1, group_index = 2 },
+    { name = "cmp_tabnine" },
+    { name = "nvim_lua" },
     { name = "luasnip", group_index = 2 },
     { name = "buffer", keyword_length = 5, group_index = 2 },
-    { name = "cmp_tabnine", group_index = 2 },
     { name = "path", group_index = 2 },
     { name = "emoji", group_index = 2 },
     { name = "lab.quick_data", keyword_length = 4, group_index = 2 },
   },
   sorting = {
+    priority_weight = 2,
+    comparators = {
+          compare.offset,
+          compare.exact,
+          compare.score,
+          compare.recently_used,
+          compare.locality,
+          compare.sort_text,
+          compare.length,
+          compare.order,
+    },
   },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
